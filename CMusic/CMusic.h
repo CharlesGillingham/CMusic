aea9bd5f0b23fcbbbfffa4299a891290c3d5e8a5
charlesgillingham <cgillingham1@mac.com>
2015-10-09 20:06:56 -0500

    Initial Commit

---
+++ CMusic/CMusic.h
@@ -0,0 +1,19 @@
+//
+//  CMusic.h
+//  CMusic
+//
+//  Created by CHARLES GILLINGHAM on 10/9/15.
+//  Copyright (c) 2015 CharlesGillingham. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CMusic.
+FOUNDATION_EXPORT double CMusicVersionNumber;
+
+//! Project version string for CMusic.
+FOUNDATION_EXPORT const unsigned char CMusicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMusic/PublicHeader.h>
+
+
